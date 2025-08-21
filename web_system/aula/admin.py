2177ab6ade3f68ba3108b65dec7bbbbab325d9ad
Tarpah <sandro_are@hotmail.com>
2025-08-21 11:58:47 -0300

    Atualizado_com_codigo_aula
    Commit da aula 21/08/2025

--- web_system/aula/admin.py
+++ web_system/aula/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from aula.models import Atividade
+
 
 # Register your models here.
+admin.site.register(Atividade)
--- web_system/aula/models/atividade.py
+++ web_system/aula/models/atividade.py
@@ -3,3 +3,55 @@ from django.db import models
 
 class Atividade(baseModel):
     nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField(verbose_name='Preço',
+                                max_digits=10,
+                                decimal_places=2,
+                                default=0.00,
+                                help_text='Digite o valor da atração em reais R$.')
+
+    turno = models.CharField(max_length=8,
+                             null=True,
+                             blank=True) # null para o back e blank para o front
+
+    nota = models.FloatField(default=0.0)
+
+    # nome: String{100}
+    # turno: String{8}
+    # nota: float
+    # duracao: time
+    # ingresso: bool
+    # valor: decimal {opcional}
+    # informações: String {text}
+    # guia: bool
+    # endereço: String
+    # participantes: int
+
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
+
+
+class Local(baseModel):
+    pass
+    # nome: String {100}
+    # endereco: String {200}
+    # nota: float
+    # horario_abertura: time
+    # horario-fechamento: time
+    # informacoes: String{text}
+    # ingresso: bool
+    # valor: decimal(opcional)
+    # acessibilidade: bool
+    # classificacao: int
+    # contato: email
+
+
+class Avaliacao(baseModel):
+    pass
+    # titulo: String {100}
+    # data_avaliacao: date
+    # data_visita: date
+    # comentario: String {text}
+    # nota: int
+    # acompanhantes: String{20}
+    # likes: int
+
