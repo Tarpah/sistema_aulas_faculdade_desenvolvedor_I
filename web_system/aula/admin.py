5cae3bdfc084367c7251e30fe0ffae38c17cb3e1
Tarpah <sandro_are@hotmail.com>
2025-08-27 00:25:54 -0300

    Preparacao codigo dia anterior

--- web_system/aula/admin.py
+++ web_system/aula/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from aula.models import Atividade
+from aula.models import Atividade, Restaurante, Local, Avaliacao
 
 
 # Register your models here.
 admin.site.register(Atividade)
+admin.site.register(Avaliacao)
+admin.site.register(Restaurante)
+admin.site.register(Local)
--- web_system/aula/models/atividade.py
+++ web_system/aula/models/atividade.py
@@ -1,9 +1,11 @@
 from .base_model import baseModel
 from django.db import models
+from datetime import date
+
 
 class Atividade(baseModel):
     nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField(verbose_name='Preço',
+    valor = models.DecimalField(verbose_name='Valor',
                                 max_digits=10,
                                 decimal_places=2,
                                 default=0.00,
@@ -14,44 +16,56 @@ class Atividade(baseModel):
                              blank=True) # null para o back e blank para o front
 
     nota = models.FloatField(default=0.0)
-
-    # nome: String{100}
-    # turno: String{8}
-    # nota: float
-    # duracao: time
-    # ingresso: bool
-    # valor: decimal {opcional}
-    # informações: String {text}
-    # guia: bool
-    # endereço: String
-    # participantes: int
+    duracao = models.DurationField(null=True, blank=True)
+    ingresso = models.BooleanField(default=False)
+    informacoes = models.TextField(null=True, blank=True)
+    guia = models.BooleanField(default=False)
+    endereco = models.TextField(null=True, blank=True)
+    participantes = models.IntegerField(default=1)
 
     def __str__(self):
         return f'{self.id} - {self.nome}'
 
-
 class Local(baseModel):
-    pass
-    # nome: String {100}
-    # endereco: String {200}
-    # nota: float
-    # horario_abertura: time
-    # horario-fechamento: time
-    # informacoes: String{text}
-    # ingresso: bool
-    # valor: decimal(opcional)
-    # acessibilidade: bool
-    # classificacao: int
-    # contato: email
+    nome = models.CharField('Nome', max_length=100, default='')
+    endereco = models.TextField(blank=False, default='', verbose_name='Endereço')
+    nota = models.FloatField(default=0.0)
+    horario_abertura = models.TimeField(null=True, blank=True, verbose_name='Horário de Abertura')
+    horario_fechamento = models.TimeField(null=True, blank=True, verbose_name='Horário de Fechamento')
+    informacoes = models.TextField(null=True, blank=True, verbose_name='Informações')
+    ingresso = models.BooleanField(default=False, verbose_name='Ingresso')
+    valor = models.DecimalField(verbose_name='Valor',
+                                max_digits=10,
+                                decimal_places=2,
+                                default=0.00,
+                                help_text='Digite o valor da atração em reais R$.')
+    acessibilidade = models.BooleanField(default=False, verbose_name='Acessibilidade')
+    classificacao = models.IntegerField(default=0, verbose_name='Classificação')
+    contato = models.EmailField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
 
 class Avaliacao(baseModel):
-    pass
-    # titulo: String {100}
-    # data_avaliacao: date
-    # data_visita: date
-    # comentario: String {text}
-    # nota: int
-    # acompanhantes: String{20}
-    # likes: int
+    titulo = models.CharField('Titulo', max_length=100, default='')
+    data_avaliacao = models.DateField(default=date.today, verbose_name='Data da Avaliação') # provavelmente é redundante, pois auto_now_add faz isso sozinho
+    data_visita = models.DateField(default=date.today, verbose_name='Data da Visita')
+    comentario = models.TextField(null=True, blank=True, verbose_name='Comentário')
+    nota = models.FloatField(default=0.0)
+    acompanhantes = models.CharField(max_length=20,
+                                    null=True,
+                                    blank=True) # questionar o professor provavelmente publico_alvo é melhor
+    likes = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return f'{self.id} - {self.titulo}'
+
+class Restaurante(baseModel):
+    nome = models.CharField('Nome', max_length=100)
+    valor = models.DecimalField(verbose_name='Valor', max_digits=10, decimal_places=2)
+    tipo_refeicao = models.CharField('Tipo Refeicao', max_length=100)
+    faixa_de_preco = models.FloatField(default=0.0)
+    aceita_reserva = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.nome}'
