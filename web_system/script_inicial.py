d50d5b0ad92b1260e322df74bbc3c7ef7c49fd04
Tarpah <sandro_are@hotmail.com>
2025-09-17 12:04:18 -0300

    Aula dia 17/09/2025

---
+++ web_system/script_inicial.py
@@ -0,0 +1,50 @@
+from django.template.defaultfilters import title
+
+from manage import *
+import contextlib, io
+
+saida = io.StringIO()
+
+with contextlib.redirect_stdout(saida):
+    main()
+
+
+# Seus imports necessários
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from relacionamentos.models import Reporter, Article, Magazine
+from datetime import date
+
+User = get_user_model()
+# User.objects.create_superuser('admin', 'admin@email.com', 'admin')
+
+
+# Exemplo de consulta e de impressão do resultado
+def main():
+    try:
+        reporter = Reporter(name='Fernando', cpf='18640053006', email='fernando@gmail.com')
+        reporter.full_clean()
+        reporter.save()
+
+    except ValidationError as e:
+        raise e
+
+# main()
+
+def consultar():
+    from relacionamentos.models import Reporter
+    lista_reporters = Reporter.objects.filter(name='Fernando')
+    lista_reporters_exata = Reporter.objects.filter(name__exact='Fernando')
+
+    for reporters in lista_reporters_exata:
+        print(f'Nome do reporter:{reporters.name}  ID:{reporters.id}')
+
+
+
+consultar()
+
+print('teste')
+
+
+
+
